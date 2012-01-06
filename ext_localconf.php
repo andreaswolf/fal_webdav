451d051be2434a0d25188e930b3c96e4ab555139
Andreas Wolf <andreas.wolf@ikt-werk.de>
2012-01-06 11:17:51 +0100

    [TASK] Move SabreDAV library to Git submodule
    Currently, the pointer is to the latest master commit in my fork on
    Github; this should be changed to the official repository (and an
    official release) as soon as the changes are included in a release.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$newPath = t3lib_extMgm::extPath('fal_webdav') . 'Resources/Php/';
+$newPath = t3lib_extMgm::extPath('fal_webdav') . 'Resources/Php/SabreDAV/lib/';
 set_include_path($newPath . PATH_SEPARATOR . get_include_path());
 
 ?>
