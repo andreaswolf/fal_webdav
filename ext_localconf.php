b699d9e856d6bafc9f17e1fc7877293eee2fbca1
Andreas Wolf <andreas.wolf@ikt-werk.de>
2012-01-09 12:23:34 +0100

    [BUGFIX] Driver registration sometimes fails

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,4 +3,8 @@
 $newPath = t3lib_extMgm::extPath('fal_webdav') . 'Resources/Php/SabreDAV/lib/';
 set_include_path($newPath . PATH_SEPARATOR . get_include_path());
 
+/** @var t3lib_file_Driver_DriverRegistry $registry */
+$registry = t3lib_div::makeInstance('t3lib_file_Driver_DriverRegistry');
+$registry->registerDriverClass('Tx_FalWebdav_Driver_WebDavDriver', 'WebDav', '', 'FILE:EXT:fal_webdav/Configuration/FlexForm/WebDavDriverFlexForm.xml');
+
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,3 @@
 <?php
 
-/** @var t3lib_file_Driver_DriverRegistry $registry */
-$registry = t3lib_div::makeInstance('t3lib_file_Driver_DriverRegistry');
-$registry->registerDriverClass('Tx_FalWebdav_Driver_WebDavDriver', 'WebDav', '', 'FILE:EXT:fal_webdav/Configuration/FlexForm/WebDavDriverFlexForm.xml');
-
 ?>
