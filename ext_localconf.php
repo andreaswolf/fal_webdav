9f066231de1f4545bf48f790b54884cc69f940af
Benjamin Mack <benni@typo3.org>
2012-04-12 15:56:25 +0200

    [CLEANUP] Cleaned up extension files

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+
 include_once t3lib_extMgm::extPath('fal_webdav') . 'Resources/Php/SabreDAV/lib/Sabre/autoload.php';
 
 $newPath = t3lib_extMgm::extPath('fal_webdav') . 'Resources/Php/SabreDAV/lib/';
@@ -10,5 +14,3 @@ $registry = t3lib_div::makeInstance('t3lib_file_Driver_DriverRegistry');
 $registry->registerDriverClass('Tx_FalWebdav_Driver_WebDavDriver', 'WebDav', 'WebDAV', 'FILE:EXT:fal_webdav/Configuration/FlexForm/WebDavDriverFlexForm.xml');
 
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:fal_webdav/Classes/Backend/TceMainHook.php:&Tx_FalWebdav_Backend_TceMainHook';
-
-?>
